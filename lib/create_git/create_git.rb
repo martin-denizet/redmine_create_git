9f9f69e4dac42e07aa0f51e850eecdac66af4433
Martin DENIZET <martin.denizet@supinfo.com>
2014-03-28 17:20:50 +0800

    Cleanup.

--- lib/create_git/create_git.rb
+++ lib/create_git/create_git.rb
@@ -26,14 +26,18 @@ class GitCreator
       repo.login = ''
       repo.password = ''
       repo.root_url = new_repo_path
+      #If the checkout plugin is installed
       if (defined?(Checkout))
+        #New checkout plugin configuration hash
         repo.checkout_settings = HashWithIndifferentAccess.new
-        repo.checkout_settings['checkout_display_command']="0"
+        #TODO: Use Checkout plugin defaults
+        repo.checkout_settings['checkout_display_command']='0'
         repo.checkout_settings['checkout_protocols']=[{'command' => 'git clone', 'is_default' => '1', 'protocol' => 'Git', 'fixed_url' => repo_url_base+new_repo_name, 'access' => 'permission'}] unless  repo_url_base.nil?
         repo.checkout_settings['checkout_description']="The data contained in this repository can be downloaded to your computer using one of several clients.\nPlease see the documentation of your version control software client for more information.\n\nPlease select the desired protocol below to get the URL.\n"
         repo.checkout_settings['checkout_overwrite']='1'
         repo.checkout_overwrite = '1'
       end
+      #TODO: Use Redmine defaults
       repo.path_encoding = ''
       repo.log_encoding = nil
       repo.extra_info = {'extra_report_last_commit' => '0'}
@@ -47,26 +51,28 @@ class GitCreator
   def self.create_repo(repo_fullpath)
     if File.exist?(repo_fullpath)
       Rails.logger.error "Repository in '#{repo_fullpath}' already exists!"
-      raise I18n.t('errors.repo_already_exists',{path:repo_fullpath})
+      raise I18n.t('errors.repo_already_exists', {path: repo_fullpath})
     else
+      #Clone the new repository to initialize it
+      #FIXME: incompatible with Windows
       temporary_clone='/tmp/tmp_create_git/'
       system("rm -Rf #{temporary_clone}")
       system("mkdir #{repo_fullpath}")
       system("cd #{repo_fullpath} && git init --bare")
       system("git clone #{repo_fullpath} #{temporary_clone}");
-      #system("cp /opt/redmine/scripts/scm-integration/gitignore #{temporary_clone}/.gitignore");
-      File.open("#{temporary_clone}/.gitignore", 'w') { |f| f.write(Setting.plugin_redmine_create_git['gitignore']) }
 
+      File.open("#{temporary_clone}/.gitignore", 'w') { |f| f.write(Setting.plugin_redmine_create_git['gitignore']) }
+      #Make first commit
+      #TODO: Make message configurable
       system("cd #{temporary_clone} && git add .gitignore && git commit -m 'First Commit' && git push origin master");
       #Create branches
       Setting.plugin_redmine_create_git['branches'].gsub(/\r/, '').split(/\n/).each do |branch|
         Rails.logger.info "Adding branch #{branch}"
         system("cd #{temporary_clone} && git checkout -b #{branch} && git push origin #{branch}");
       end
+      #Delete the temporary clone
       system("rm -Rf  #{temporary_clone}")
-      #system("chown -R www-data:www-data #{repo_fullpath}")
-      #puts "Installing hooks"
-      #system('/opt/redmine/scripts/scm-integration/hook-installer.pl > /dev/null')
+
       Rails.logger.info 'Creation finished'
     end
     return true
