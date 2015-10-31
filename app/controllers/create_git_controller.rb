d36ffe95aa5447e10f34d2ec1b4db1ef00a18995
Martin DENIZET <martin.denizet@supinfo.com>
2015-10-31 15:28:04 +0100

    Redmine 3.x compat and minor changes.

--- app/controllers/create_git_controller.rb
+++ app/controllers/create_git_controller.rb
@@ -9,8 +9,10 @@ class CreateGitController < ApplicationController
   def new
 
     @repo_path_base = Setting.plugin_redmine_create_git['repo_path']
-    @repo_path_base += '/' unless @repo_path_base[-1, 1]=='/'
-    @repo_path_base += @project.identifier
+    unless @repo_path_base.nil? or @repo_path_base.empty?
+      @repo_path_base += '/' unless @repo_path_base[-1, 1]=='/'
+      @repo_path_base += @project.identifier
+    end
 
   end
 
@@ -41,9 +43,9 @@ class CreateGitController < ApplicationController
 
   def check_settings
     repo_path = Setting.plugin_redmine_create_git['repo_path']
-    return flash[:error] = I18n.t('errors.repo_path_undefined') if repo_path.empty?
-    return flash[:error] = I18n.t('repo_path_doesnt_exist', {path: repo_path}) unless File.exist?(repo_path)
-    return flash[:error] = I18n.t('repo_path_not_writable', {path: repo_path}) unless (File.exist?(repo_path) and File.stat(repo_path).writable_real?)
+    return flash[:error] = I18n.t('errors.repo_path_undefined') if repo_path.nil? or repo_path.empty?
+    return flash[:error] = I18n.t('repo_path_doesnt_exist', {:path => repo_path}) unless File.exist?(repo_path)
+    return flash[:error] = I18n.t('repo_path_not_writable', {:path => repo_path}) unless (File.exist?(repo_path) and File.stat(repo_path).writable_real?)
   end
 
   def check_create_permission
